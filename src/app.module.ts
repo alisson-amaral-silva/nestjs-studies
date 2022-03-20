9f2f376c65fc8595fecbf1ed5d3b0d1bbc1fec82
Alisson Amaral <aalissonamaral98@gmail.com>
2022-03-20 17:57:16 -0300

    add environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,17 @@ import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Book } from './book.model';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'Fr@t3rn1d4d3',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       database: 'book_store',
       autoLoadModels: true, //* recognize our project models
       synchronize: true, //* if there is no book table, it'll be created
