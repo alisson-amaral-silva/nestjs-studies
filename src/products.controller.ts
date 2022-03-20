b4e491d9a9008908dc5a026f64391c6e9133ef89
Alisson Amaral <aalissonamaral98@gmail.com>
2022-03-20 16:10:44 -0300

    add product model

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { Product } from './product.model';
 
 @Controller('products')
 export class ProductsController {
